d5445e4c7ebd6d6533a67542040da3ac5e19dc91
Guilherme Anzollin <guilherme.anzollin@codelabore.com>
2019-08-05 15:48:06 -0300

    Added application files

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <GL/glut.h>  // GLUT, include glu.h and gl.h
+
+void init(){
+	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+	glutInitWindowSize(640,480);
+	glutInitWindowPosition(0,0);
+	glutCreateWindow("My GUI App");
+}
+
+void display(){
+	glClearColor(1.0,1.0,1.0,0.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glFlush();
+}
+
+int main(int argc,char **argv){
+	glutInit(&argc,argv);
+	init();
+	glutDisplayFunc(display);
+	glutMainLoop();	
+	return 0;
+}
